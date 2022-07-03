451c2837c0072118d7e2662ff8083704e4e79182
Aliza02 <alizaanxari08@gmail.com>
2022-07-03 11:44:09 +0500

    design complete

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,12 @@
+const submit=document.querySelector('.submit-btn');
+const thankyou_container=document.querySelector('.thankyou-container');
+const card=document.querySelector('.container-fluid');
+submit.addEventListener('click',()=>{
+    // document.getElementsById("hj").style.display='block';
+ 
+
+  card.style.display='none';
+  thankyou_container.style.display='block';
+
+    console.log(card);    
+})
